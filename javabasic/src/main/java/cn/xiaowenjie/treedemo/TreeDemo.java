3d9d5a190a3947d723f5b28face2bd0a53b6eaea
晓风轻 <121509092@qq.com>
2019-04-07 23:22:11 +0800

    中序遍历非递归

--- javabasic/src/main/java/cn/xiaowenjie/treedemo/TreeDemo.java
+++ javabasic/src/main/java/cn/xiaowenjie/treedemo/TreeDemo.java
@@ -2,45 +2,49 @@ package cn.xiaowenjie.treedemo;
 
 import java.util.Deque;
 import java.util.LinkedList;
-import java.util.List;
+import java.util.Stack;
 
 public class TreeDemo {
     public static void main(String[] args) {
         // 按层遍历顺序构建树
-        TreeNode<Integer> root = Tree.<Integer>buildTreee(1, 2, 3, 4, 5, 6, 7, 8);
+        TreeNode<Integer> root = Tree.<Integer>buildTreee(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
         // 层遍历非递归
+        System.out.println("层遍历非递归");
         levelWalk(root);
         System.out.println();
 
         // 递归中序遍历
+        System.out.println("递归中序遍历");
         middleWalk(root);
         System.out.println();
 
         // 非递归中序遍历
-//        middleWalk2(root);
-//        System.out.println();
-
+        System.out.println("非递归中序遍历");
+        middleWalk2(root);
+        System.out.println();
     }
 
     /**
      * 中序遍历
+     *
      * @param node
      */
     private static void middleWalk(TreeNode<Integer> node) {
-        if(node.left != null){
+        if (node.left != null) {
             middleWalk(node.left);
         }
 
         System.out.print(node.value + ",");
 
-        if(node.right != null){
+        if (node.right != null) {
             middleWalk(node.right);
         }
     }
 
     /**
      * 层遍历非递归
+     *
      * @param root
      */
     private static void levelWalk(TreeNode<Integer> root) {
@@ -48,7 +52,7 @@ public class TreeDemo {
 
         que.add(root);
 
-        while(!que.isEmpty()) {
+        while (!que.isEmpty()) {
 
             TreeNode<Integer> node = que.pop();
 
@@ -66,25 +70,29 @@ public class TreeDemo {
 
     /**
      * 中序遍历非递归
+     * <p>
+     * 1)若其左孩子不为空，则将P入栈并将P的左孩子置为当前的P，然后对当前结点P再进行相同的处理；
+     * 2)若其左孩子为空，则取栈顶元素并进行出栈操作，访问该栈顶结点，然后将当前的P置为栈顶结点的右孩子；
+     * 3)直到P为NULL并且栈为空则遍历结束
+     *
      * @param root
      */
     private static void middleWalk2(TreeNode<Integer> root) {
-        Deque<TreeNode<Integer>> que = new LinkedList<TreeNode<Integer>>();
+        Stack<TreeNode<Integer>> stack = new Stack<TreeNode<Integer>>();
+        TreeNode<Integer> node = root;
 
-        que.add(root);
+        while (node != null || !stack.empty()) {
+            while (node != null) {
+                stack.push(node);
 
-        while(!que.isEmpty()) {
-
-            TreeNode<Integer> node = que.peek();
-
-            if (node.left != null) {
-                que.addFirst(node.left);
+                node = node.left;
             }
 
-            System.out.print(node.value + ",");
+            if (!stack.empty()) {
+                node = stack.pop();
+                System.out.print(node.value + ",");
 
-            if (node.right != null) {
-                que.add(node.right);
+                node = node.right;
             }
         }
     }
